2527574ece4769da8ca98ce82a27b15549211f3c
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-10-08 13:57:56 +0200

    update5
    make search person more better using Map

--- Main.java
+++ Main.java
@@ -3,22 +3,23 @@ package simpleSearchEngine;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 	static Scanner sc = new Scanner(System.in);
 	static ArrayList<String> arr = new ArrayList<String>();
+	static Map<String, List<Integer>> words = new HashMap<String, List<Integer>>();
 	
 	public static void main(String[] args) {
-		String[] allPeople = null;
 		if(args.length>1)
 			if(args[0].equals("--data")) {
 				getDataFile(args[1]);
-				allPeople = new String[arr.size()];
-				for (int i = 0; i < allPeople.length; i++) 
-					allPeople[i] = arr.get(i);
 			}
-		if(allPeople.length != 0) {
+		if(arr.size() != 0) {
+			indexingWord();
 			sc = new Scanner(System.in);
 			int fin = 0;
 			while(fin == 0) {
@@ -26,10 +27,10 @@ public class Main {
 				int ch = Integer.valueOf(sc.nextLine());
 				switch (ch) {
 				case 1:
-					search(allPeople);
+					search();
 					break;
 				case 2:
-					listePeople(allPeople);
+					listePeople();
 					break;
 				case 0:
 					fin = 1;
@@ -49,19 +50,22 @@ public class Main {
 		System.out.println("2. Print all data.");
 		System.out.println("0. Exit.");
 	}
-	static void search(String[] arr) {
+	static void search() {
 		System.out.println("\nEnter a name or email to search all suitable people.");
 		String name = sc.nextLine().toLowerCase();
-		for (int i = 0; i < arr.length; i++) {
-			String s = arr[i].toLowerCase();
-			if(s.matches(".*" + name + ".*"))
-				System.out.println(arr[i]);
+		if(words.containsKey(name)) {
+			List<Integer> index = words.get(name);
+			for (Integer i : index) {
+				System.out.println(arr.get(i));
+			}
 		}
+		else
+			System.out.println("No matching people found.");
 	}
-	static void listePeople(String[] arr) {
+	static void listePeople() {
 		System.out.println("\n=== List of people ===");
-		for (int i = 0; i < arr.length; i++) 
-			System.out.println(arr[i]);
+		for (int i = 0; i < arr.size(); i++) 
+			System.out.println(arr.get(i));
 	}
 
 	static void getDataFile(String nameFile) {
@@ -78,85 +82,25 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-}
 
-
-
-
-/*
-public static void main(String[] args) {
-String[] allPeople = null;
-if(args.length>1)
-	if(args[0].equals("--data")) {
-		getDataFile(args[1]);
-		allPeople = new String[arr.size()];
-		for (int i = 0; i < allPeople.length; i++) 
-			allPeople[i] = arr.get(i);
-	}
-else {
-	System.out.println("Enter the number of people:");
-	int nbrPeople = Integer.valueOf(sc.nextLine());
-	System.out.println("Enter all people:");
-	allPeople = new String[nbrPeople];
-	for (int i = 0; i < allPeople.length; i++) 
-		allPeople[i] = sc.nextLine();
-}
-
-
-sc = new Scanner(System.in);
-int fin = 0;
-while(fin == 0) {
-	menu();
-	int ch = Integer.valueOf(sc.nextLine());
-	switch (ch) {
-	case 1:
-		search(allPeople);
-		break;
-	case 2:
-		listePeople(allPeople);
-		break;
-	case 0:
-		fin = 1;
-		System.out.println("\nBye!");
-		break;
-	default:
-		System.out.println("\nIncorrect option! Try again.");
+	static void indexingWord() {
+		ArrayList<String> allWord = new ArrayList<String>(); 
+		for (int i = 0; i < arr.size(); i++) {
+			String[] s = arr.get(i).split(" ");
+			for (int j = 0; j < s.length; j++) 
+				allWord.add(s[j].toLowerCase());
+		}
+		//
+		for (int i = 0; i < allWord.size(); i++) {
+			if(!words.containsKey(allWord.get(i))) {
+				List<Integer> index = new ArrayList<Integer>();
+				for (int j = 0; j < arr.size(); j++) {
+					String ss = arr.get(j).toLowerCase();
+					if(ss.matches(".*" + allWord.get(i) + ".*"))
+						index.add(j);
+				}
+				words.put(allWord.get(i), index);
+			}
+		}
 	}
 }
-}
-
-static void menu() {
-System.out.println("\n=== Menu ===");
-System.out.println("1. Search information.");
-System.out.println("2. Print all data.");
-System.out.println("0. Exit.");
-}
-static void search(String[] arr) {
-System.out.println("\nEnter a name or email to search all suitable people.");
-String name = sc.nextLine().toLowerCase();
-
-for (int i = 0; i < arr.length; i++) {
-	String s = arr[i].toLowerCase();
-	if(s.matches(".*" + name + ".*"))
-		System.out.println(arr[i]);
-}
-}
-static void listePeople(String[] arr) {
-System.out.println("\n=== List of people ===");
-for (int i = 0; i < arr.length; i++) 
-	System.out.println(arr[i]);
-}
-
-static void getDataFile(String nameFile) {
-try {
-	File f = new File("C:\\Users\\HP\\eclipse-work\\zhard8\\src\\simpleSearchEngine\\" + nameFile);
-	sc = new Scanner(f);
-	while(sc.hasNext()) 
-		arr.add(sc.nextLine());
-} 
-catch (IOException e) { 
-	e.printStackTrace();
-}
-}
-}
-*/
